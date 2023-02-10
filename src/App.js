b3af4ced3d8558d303a41f48c766b38430d0cd9f
VivianC <viviancao58@gmail.com>
2023-02-10 18:14:39 +1100

    create counter buttons with useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Counter</h1>
+      <span id="value">{count}</span>
+      <div className="button-container">
+        <button className="btn decrease" onClick={decrease}>
+          decrease
+        </button>
+        <button className="btn reset" onClick={reset}>
+          reset
+        </button>
+        <button className="btn increase" onClick={increase}>
+          increase
+        </button>
+      </div>
+    </main>
   );
 }
 
